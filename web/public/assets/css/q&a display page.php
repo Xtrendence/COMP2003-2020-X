e339c54ca096c2240f0b420cd8892c99dee49f59
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2020-11-19 14:44:51 +0000

    Q&A page created
    Q&A page created, along with its custom css. An addition to global made

---
+++ web/public/assets/css/q&a display page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./themes.css">
+    <link rel="stylesheet" href="./global.css">
+    <link rel="stylesheet" href="./q&a%20display%20page.css">
+    <meta charset="UTF-8">
+    <title>Landing page</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
