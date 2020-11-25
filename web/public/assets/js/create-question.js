ea2ced2120bf26887e0b86c2ce22647779e84123
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2020-11-25 15:28:08 +0000

    adding html to php

--- web/public/assets/js/create-question.js
+++ web/public/assets/js/create-question.js
@@ -2,18 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let multipleChoiceOption = document.getElementById("multiple-choice-op");
     let option = document.getElementById("multiple");
+    let choices = document.getElementById("number-of-choices");
 
     multipleChoiceOption.addEventListener("click", function() {
 
         if (multipleChoiceOption.className === "form-radio-button") {
             option.classList.remove("hidden");
-
             multipleChoiceOption.classList.add("active");
 
         } else {
             option.classList.add("hidden");
-
             multipleChoiceOption.classList.remove("active");
         }
+    });
+
+    choices.addEventListener("keyup", function(){
+
     });
 });
