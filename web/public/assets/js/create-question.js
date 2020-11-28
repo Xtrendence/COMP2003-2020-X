5c484ae9d1390df8442a544ff34e12beee3abbfc
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2020-11-28 16:18:08 +0000

    JS comments added

--- web/public/assets/js/create-question.js
+++ web/public/assets/js/create-question.js
@@ -20,15 +20,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let submitButton = document.getElementById("submit");
     let submitText = document.getElementById("sub");
 
-
+    /**
+     * @desc event listener that looks for the click on the multiple choice radio button
+     *      it clears all the input boxes, not the question box, and shows all the relevant information boxes to be
+     *      filled in
+     */
     multipleChoiceRadioButton.addEventListener("click", function() {
         numberOfChoices.value = null;
-
         multipleOption.classList.remove("hidden");
         multipleChoiceRadioButton.classList.add("active");
         longAnswerText.classList.add("hidden");
         longAnswerRadioButton.classList.remove("active");
         multipleChoiceRadioButton.classList.add("active");
+
         if (optionContainer.children.length > 0){
             while (optionContainer.hasChildNodes()) {
                 optionContainer.removeChild(optionContainer.lastElementChild);
@@ -41,9 +45,13 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    /**
+     * @desc event listener that looks for the click on the long answer radio button
+     *      it clears all the input boxes, not the question box, and shows all the relevant information boxes to be
+     *      filled in
+     */
     longAnswerRadioButton.addEventListener("click", function() {
         characterLimit.value = null;
-
         longAnswerText.classList.remove("hidden");
         longAnswerRadioButton.classList.add("active");
         multipleChoiceRadioButton.classList.remove("active");
@@ -56,9 +64,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    /**
+     * @desc event listener waits for keyup then checks to see if the key pressed is back space,
+     *      once that is checked, it removes all the child nodes from the option container where the
+     *      number of choices are larger than 2 and 16 or less
+     */
     numberOfChoices.addEventListener("keyup", function(e){
         if (e.key.toLowerCase() === "backspace") {
-            if (numberOfChoices.value > 3 || numberOfChoices.value <= 16) {
+            if (numberOfChoices.value > 2 || numberOfChoices.value <= 16) {
                 while (optionContainer.hasChildNodes()) {
                     optionContainer.removeChild(optionContainer.lastElementChild);
                 }
@@ -66,10 +79,16 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    /**
+     * @desc event listener awaits key press and checks if it was the enter button.
+     *      when this is true it will then check to see if the value is 16 or less,
+     *      when this is the case it will add the appropriate number of choice boxes into
+     *      the option container. if not it will highlight the box with an error
+     */
     numberOfChoices.addEventListener("keypress", function(e){
         if (e.key.toLowerCase() === "enter") {
-            numberOfChoices.classList.remove("error");
             if (numberOfChoices.value <= 16) {
+                numberOfChoices.classList.remove("error");
                 for (let i = 2; i < (numberOfChoices.value); i++) {
                     let input = document.createElement("input");
                     input.type = "text";
@@ -77,13 +96,24 @@ document.addEventListener("DOMContentLoaded", () => {
                     input.placeholder = "Choice " + (i + 1) + "...";
                     optionContainer.appendChild(input);
                 }
-            }else {
+            } else {
                 numberOfChoices.classList.add("error");
             }
         }
 
     });
 
+    /**
+     * @desc event listener awaits click on the submit button. The first thing it does
+     *      is check to see which radio button is active. If it's the multiple choice radio
+     *      button then it'll enter al the users custom choices into an array, after that it
+     *      constructs the object to be used in the api. This contains the question, type of question
+     *      and the choices. Then all of the input boxes are cleared, and a note saying that the
+     *      question has been submitted. If the long answer radio button is selected, it checks to
+     *      make sure the character limit is no less than 50, if it is, the input box will
+     *      be highlighted red. Once corrected, it creates the object containing the question, type of question
+     *      and the character limit.
+     */
     submitButton.addEventListener("click", function(){
         if (multipleChoiceRadioButton.classList.contains("active")){
             let choiceOptions = []
@@ -93,29 +123,26 @@ document.addEventListener("DOMContentLoaded", () => {
             }
 
             let questionInformation = {"Question" : enquiry.value , "Type" : "Multiple Choice", "Choices" : choiceOptions};
+
             Object.keys(questionInformation).forEach(key => {
                 console.table(key, questionInformation[key]);
             });
 
             submitText.classList.remove("hidden");
             submitText.classList.add("submission");
-
             enquiry.value = "";
             numberOfChoices.value = "";
             for (let i = 0; i < 2; i++) {
                 choiceFields[i].value = "";
             }
+
             while (optionContainer.hasChildNodes()) {
                 optionContainer.removeChild(optionContainer.lastElementChild);
             }
-
-
-        }
-        else{
+        } else {
             if (characterLimit.value < 50) {
                 characterLimit.classList.add("error");
-            }
-            else{
+            } else {
                 let questionInformation = {"Question" : enquiry.value , "Type" : "Long Answer", "Length" : characterLimit.value};
                 submitText.classList.remove("hidden");
                 submitText.classList.add("submission");
@@ -126,6 +153,5 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
             }
         }
-
     });
 });
