08925e8a4cb71c3e5c563a6d3f8a8688559e144f
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2020-12-02 21:41:43 +0000

    Started designing the falls page
    added the features of the falls page

--- app/screens/FallsPage.js
+++ app/screens/FallsPage.js
@@ -1,13 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { globalColors, globalStyles } from '../styles/global';
+import { ScrollView, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { globalColors, globalStyles, globalComponentStyles } from '../styles/global';
 import { TopBar } from '../components/TopBar';
+import Card from '../components/Card';
 
 export const FallsPage = ({ navigation }) => {
+
+	const [falls, setFalls] = React.useState();
+
 	return (
 		<View>
 			<TopBar>Recording Falls</TopBar>
+			<ScrollView style={styles.cardContainer} contentContainerStyle={{paddingBottom: 20}}>
+				<Card>
+					<Text style={globalComponentStyles.cardTitle}>Today's Number of Falls</Text>
+					<TextInput style={globalComponentStyles.inputField} placeholder="Number..." multiline={false} keyboardType="numeric" onChangeText={(value) => setFalls(value)} value={falls}></TextInput>
+					<View style={styles.buttonWrapper}>
+						<TouchableOpacity style={styles.actionButton} onPress={() => saveAnswer(1, answers[1])}>
+							<Text style={styles.actionText}>Confirm</Text>
+						</TouchableOpacity>
+					</View>
+				</Card>
+			</ScrollView>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "flex-start",
+		backgroundColor: globalColors.mainSecond,
+		width: "100%",
+	},
+	cardContainer: {
+		width: "100%",
+		height: "100%",
+		paddingLeft: 20,
+	},
+	buttonWrapper: {
+		width: "100%",
+		marginTop: 15,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	actionButton: {
+		backgroundColor: globalColors.accentDark,
+		width: 80,
+		height: 35,
+		justifyContent: "center",
+		borderRadius: globalStyles.borderRadius,
+	},
+	actionText: {
+		fontSize: globalStyles.mediumFont,
+		fontWeight: "bold",
+		color: globalColors.accentContrast,
+		textAlign: "center"
+	},
+});
