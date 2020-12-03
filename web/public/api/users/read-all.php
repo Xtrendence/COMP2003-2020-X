1c0ddadeb5fc9bf99d8a4f248fe17a1a177b5958
Xtrendence <xtrendence@yahoo.com>
2020-12-03 05:04:23 +0000

    The API is now prepared to fetch all users.

---
+++ web/public/api/users/read-all.php
@@ -0,0 +1,49 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+
+	include_once '../config/Database.php';
+	include_once '../models/User.php';
+
+	$database = new Database();
+	$db = $database->connect();
+
+	$user = new User($db);
+
+	$result = $user->readAll();
+
+	$rows = $result->rowCount();
+
+	if($rows > 0) {
+		$array = array();
+		$array['data'] = array();
+		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			extract($row);
+
+			$item = array(
+				'patientID' => $patientID,
+				'patient_nhsRef' => $patient_nhsRef,
+				'patient_username' => $patient_username,
+				'patient_password' => $patient_password,
+				'patient_fName' => $patient_fName,
+				'patient_lName' => $patient_lName,
+				'patient_dob' => $patient_dob,
+				'patient_addressI' => $patient_addressI,
+				'patient_addressII' => $patient_addressII,
+				'patient_postcode' => $patient_postcode,
+				'patient_tel' => $patient_tel,
+				'patient_mobile' => $patient_mobile,
+				'patient_email' => $patient_email,
+				'patient_comment' => $patient_comment,
+				'fcmToken' => $fcmToken,
+				'fcmToken_creation' => $fcmToken_creation
+			);
+
+			array_push($array['data'], $item);
+		}
+
+		echo json_encode($array);
+	} else {
+		echo json_encode(array('message' => 'No users found.'));
+	}
+?>
