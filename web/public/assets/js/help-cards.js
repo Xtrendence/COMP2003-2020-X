ebaf636cfeef289c43ca5b88d2743066fc09b96e
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2020-12-05 13:01:16 +0000

    Making changes to JS
    Making changes to JS, will need to chnage later. Experimenting with event listener method to try and get the span tag caret class text to show when the body text class is clicked on the web page.

--- web/public/assets/js/help-cards.js
+++ web/public/assets/js/help-cards.js
@@ -1,17 +1,48 @@
+
+/** variables made from id's in the help.php
+ * all are in order as seen in the php file
+ */
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    /** variables made from id's in the help.php
-     * all are in order as seen in the php file
-    */
 
 
-    let caret1 = document.getElementById("first");
-    let caret2 = document.getElementById("second");
-    let caret3 = document.getElementById("third");
-    let caret4 = document.getElementById("fourth");
-    let caret5 = document.getElementById("fifth");
-    let caret6 = document.getElementById("sixth");
-    let caret7 = document.getElementById("seventh");
+
+    let caret1 = document.getElementsByTagName("body-text1");
+    let caret2 = document.getElementsByTagName("body-text2");
+    let caret3 = document.getElementsByTagName("body-text3");
+    let caret4 = document.getElementsByTagName("body-text4");
+    let caret5 = document.getElementsByTagName("body-text5");
+    let caret6 = document.getElementsByTagName("body-text6");
+    let caret7 = document.getElementsByTagName("body-text7");
+
+
+
+    let reveal1 = document.getElementsByClassName("caret-text1");
+    let reveal2 = document.getElementsByClassName("caret-text2");
+    let reveal3 = document.getElementsByClassName("caret-text3");
+    let reveal4 = document.getElementsByClassName("caret-text4");
+    let reveal5 = document.getElementsByClassName("caret-text5");
+    let reveal6 = document.getElementsByClassName("caret-text6");
+    let reveal7 = document.getElementsByClassName("caret-text7");
+
+
+
+    reveal1.addEventListener("click", () => {
+        caret1.classList.remove("hidden");
+        caret2.classList.add("hidden");
+        caret3.classList.add("hidden");
+        caret4.classList.add("hidden");
+        caret5.classList.add("hidden");
+        caret6.classList.add("hidden");
+        caret7.classList.add("hidden");
+
+    });
+
+
+
+
 });
 
 
