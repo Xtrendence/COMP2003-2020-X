4ad520d2d357344bd3c0c73d641cbf8ab53fc3fe
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2020-12-10 12:47:05 +0000

    Adding collapsible JS
    Removed orginal way off using event listener methods for the caret drop down text. For trial and error reasons I have tested a new way for the first title in the first top wide card. Have made the title a button so that when the button is cliked on the DIV class "caret-content" will be revealed containg the span tage text which is the answer text.

--- web/public/assets/js/help-cards.js
+++ web/public/assets/js/help-cards.js
@@ -1,48 +1,23 @@
 
-/** variables made from id's in the help.php
+/** variables made from Classes in the help.php
  * all are in order as seen in the php file
  */
 
+let drop = document.getElementsByClassName("drop-down") ;
+let i;
 
-document.addEventListener("DOMContentLoaded", () => {
-
-
-
-
-    let caret1 = document.getElementsByTagName("body-text1");
-    let caret2 = document.getElementsByTagName("body-text2");
-    let caret3 = document.getElementsByTagName("body-text3");
-    let caret4 = document.getElementsByTagName("body-text4");
-    let caret5 = document.getElementsByTagName("body-text5");
-    let caret6 = document.getElementsByTagName("body-text6");
-    let caret7 = document.getElementsByTagName("body-text7");
-
-
-
-    let reveal1 = document.getElementsByClassName("caret-text1");
-    let reveal2 = document.getElementsByClassName("caret-text2");
-    let reveal3 = document.getElementsByClassName("caret-text3");
-    let reveal4 = document.getElementsByClassName("caret-text4");
-    let reveal5 = document.getElementsByClassName("caret-text5");
-    let reveal6 = document.getElementsByClassName("caret-text6");
-    let reveal7 = document.getElementsByClassName("caret-text7");
-
-
-
-    reveal1.addEventListener("click", () => {
-        caret1.classList.remove("hidden");
-        caret2.classList.add("hidden");
-        caret3.classList.add("hidden");
-        caret4.classList.add("hidden");
-        caret5.classList.add("hidden");
-        caret6.classList.add("hidden");
-        caret7.classList.add("hidden");
-
+    for (i = 0; i < drop.length; i++) {
+        drop[i].addEventListener("click", function() {
+        this.classList.toggle("active") ;
+        let content = this.nextElementSibling ;
+        if (content.style.display === "block") {
+            content.style.display = "none" ;
+        } else {
+          content.style.display = "block" ;
+        }
     });
+}
 
 
-
-
-});
 
 
