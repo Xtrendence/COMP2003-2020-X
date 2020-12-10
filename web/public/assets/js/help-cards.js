641de80fde99d29be7a3a45feca2431a0e541bdc
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2020-12-10 13:48:12 +0000

    Solved JS collapsible issue
    Solved issue with making collapsible work for each of the title question drop downs. Within the JS file i forgot to include "document.addEventListener("DOMContentLoaded", () => {" at the start of the script. Will now apply to the other six wild cards on the page by implementing the button tag for the question titles.

--- web/public/assets/js/help-cards.js
+++ web/public/assets/js/help-cards.js
@@ -1,23 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-/** variables made from Classes in the help.php
- * all are in order as seen in the php file
- */
-
-let drop = document.getElementsByClassName("drop-down") ;
-let i;
+    let drop = document.getElementsByClassName("drop-down");
+    let i;
 
     for (i = 0; i < drop.length; i++) {
         drop[i].addEventListener("click", function() {
-        this.classList.toggle("active") ;
-        let content = this.nextElementSibling ;
-        if (content.style.display === "block") {
-            content.style.display = "none" ;
-        } else {
-          content.style.display = "block" ;
-        }
-    });
-}
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
 
 
 
 
+});
+
+
+
+
+
+
+
+    /** variables made from Classes in the help.php
+     * all are in order as seen in the php file
+     */
