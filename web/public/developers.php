4d9736db1195ca705191496d15a820bdc91dd4d1
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2020-12-11 15:45:11 +0000

    Finishing off
    Have checked code indentation for HTML. Have code commented CSS file for this specific web page (PHP) file.

--- web/public/developers.php
+++ web/public/developers.php
@@ -6,17 +6,15 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="../public/assets/css/themes.css">
-    <link rel="stylesheet" href="../public/assets/css/global.css">
-    <link rel="stylesheet" href="../public/assets/css/developers/style.css">
-
+    <link rel="stylesheet" href="./assets/css/themes.css">
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/developers/style.css">
     <meta charset="UTF-8">
     <title>Developers Page</title>
 </head>
 
 <body>
 <div class="navbar">
-
 
     <a href="index.php"><button>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"/></svg>Home
@@ -42,94 +40,63 @@
 
 <div class="wrapper">
 
-
-
     <div class="small-card">
-
         <div class="text-margin">
             <span class="card-name-text">Jazib Khan</span>
             <span class="card-role-text">Product Owner</span>
         </div>
-
         <div>
             <svg class="developer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
         </div>
-
         <div class="github-margin">
             <a href="https://github.com/Jazib-Khan"><span class="github-text">Visit GitHub</span></a>
         </div>
     </div>
 
-
-
-
-
     <div class="small-card">
-
         <div class="text-margin">
             <span class="card-name-text">Shaun Howarth</span>
             <span class="card-role-text">Product Owner</span>
         </div>
-
         <div>
             <svg class="developer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
         </div>
-
         <div class="github-margin">
             <a href="https://github.com/shaun-howarth"><span class="github-text">Visit GitHub</span></a>
         </div>
     </div>
 
-
-
     <div class="small-card">
-
         <div class="text-margin">
             <span class="card-name-text">Joshua Bent</span>
             <span class="card-role-text">Scrum Master</span>
         </div>
-
         <div>
             <svg class="developer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
         </div>
-
         <div class="github-margin">
             <a href="https://github.com/JoshBent"><span class="github-text">Visit GitHub</span></a>
         </div>
     </div>
 
-
-
-
-
     <div class="small-card">
-
         <div class="text-margin">
             <span class="card-name-text">Stephanie Skinner</span>
             <span class="card-role-text">Scrum Master</span>
         </div>
-
         <div>
             <svg class="developer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
         </div>
-
         <div class="github-margin">
             <a href="https://github.com/La-Ola"><span class="github-text">Visit GitHub</span></a>
         </div>
     </div>
 
-
-
-
-
-
     <div class="small-card">
-
         <div class="text-margin">
             <span class="card-name-text">Bethany Ward</span>
             <span class="card-role-text">Tech Lead</span>
         </div>
-
         <div>
             <svg class="developer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
         </div>
@@ -138,16 +105,11 @@
         </div>
     </div>
 
-
-
-
-
     <div class="small-card">
         <div class="text-margin">
             <span class="card-name-text">Khodadad Nouchin</span>
             <span class="card-role-text">Tech Lead</span>
         </div>
-
         <div>
             <svg class="developer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
         </div>
@@ -155,8 +117,6 @@
             <a href="https://github.com/Xtrendence"><span class="github-text">Visit GitHub</span></a>
         </div>
     </div>
-
-
 
 </div>
 
