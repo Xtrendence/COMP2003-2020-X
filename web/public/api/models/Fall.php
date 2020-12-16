993e95a1b57656f5783e2e38800d5bd6039f3eff
Xtrendence <xtrendence@yahoo.com>
2020-12-16 15:45:11 +0000

    Fixed API "read-date" endpoints.

--- web/public/api/models/Fall.php
+++ web/public/api/models/Fall.php
@@ -50,7 +50,7 @@
 		}
 
 		public function readDate($from, $to) {
-			$query = 'SELECT * FROM ' . $this->table . ' WHERE fall_date BETWEEN :from AND :to WHERE patientID=:id';
+			$query = 'SELECT * FROM ' . $this->table . ' WHERE fall_date BETWEEN :from AND :to AND patientID=:id';
 			$command = $this->connection->prepare($query);
 			$command->bindParam(':from', $from);
 			$command->bindParam(':to', $to);
