1f46044825327cf28195a3a1f215d353ee74e7bb
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2020-12-18 18:00:42 +0000

    Made final adjustments to JS code and other content
    Have made final adjustments to JS code for the collaspible drop down text on the help page. Was missing this suitable line of code "this.getElementsByClassName("caret-icon")[0].classList.toggle("active");" that toggles the active.caret class to turn on, causing the caert svg to tun 90 degrees downward on click drop down with the text. Adjusted the bordering of the drop down text better. Presentation now looks more in proportion.

--- web/public/assets/js/help-cards.js
+++ web/public/assets/js/help-cards.js
@@ -1,19 +1,15 @@
-
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     /** variable made from button Class "drop-down" in the help.php */
 
     let drop = document.getElementsByClassName("drop-down");
-    let i;
 
-    let caret = document.getElementsByClassName("caret-icon");
+    /** Event listener included within for loop for the button to reveal the hidden text and turn caret svg 90 degrees right */
 
-    /** Event listener included within for loop for the button to reveal the hidden text */
-
-    for (i = 0; i < drop.length; i++) {
+    for (let i = 0; i < drop.length; i++) {
         drop[i].addEventListener("click", function() {
             this.classList.toggle("active");
-            caret[i].classList.toggle("active");
+            this.getElementsByClassName("caret-icon")[0].classList.toggle("active");
             let content = this.nextElementSibling;
             if (content.style.display === "block") {
                 content.style.display = "none";
