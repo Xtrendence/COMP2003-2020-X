d62c53cd2cf52970c1eaf4d580107ada7833a964
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2020-12-23 20:08:55 +0000

    Create() account for choices
    choices can now  to be run and accounted for

--- web/public/api/models/Question.php
+++ web/public/api/models/Question.php
@@ -7,18 +7,32 @@
         public $question;
         public $question_charLim;
         public $question_type;
+        public $choices;
 
         public function __construct($db) {
 			$this->connection = $db;
         }
         
         public function create() {
-            $query = 'INSERT INTO ' . $this->table . ' (question, question_type, question_charLim) VALUES (:question, :question_type, :question_carLim)';
-            $command = $this->connection->prepare($query);
-			$command->bindParam(':question', $this->question);
-			$command->bindParam(':question_type', $this->question_type;
-            $command->bindParam(':question_charLim', $this->question_charLim);
-            $command->execute(); 
+            if($this->question_type == 'custom') {
+                $query = 'INSERT INTO ' . $this->table . ' (question, question_type, question_charLim) VALUES (:question, :question_type, :question_carLim)';
+                $command = $this->connection->prepare($query);
+                $command->bindParam(':question', $this->question);
+                $command->bindParam(':question_type', $this->question_type);
+                $command->bindParam(':question_charLim', $this->question_charLim);
+                $command->execute();
+
+            }else {
+                for($i = 0; $i <= count($choices); $i++ ) {
+                    $query = 'INSERT INTO choice (questionID, choice) VALUES (:questionID, :choice)';
+                    $command = $this->connection->prepare($query);
+                    $command->bindParam(':questionID', $this->questionID);
+                    $command->bindParam(':choice', $choices[$i]);
+                    $command->execute();
+                }
+
+            }
+            
         }
 
         public function delete() {
