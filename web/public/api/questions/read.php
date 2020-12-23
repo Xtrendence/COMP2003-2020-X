35011a1dcaaa98bc3d361f45188d5694dc4399e0
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2020-12-23 22:15:59 +0000

    read.php
    read.php takes questionID and displays information about it

---
+++ web/public/api/questions/read.php
@@ -0,0 +1,49 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+		include_once '../config/Database.php';
+		include_once '../models/Question.php';
+
+		$api_key = isset($_GET['key']) ? $_GET['key'] : die(json_encode(array('message' => 'No API key provided.')));
+
+		$database = new Database();
+		$db = $database->connect($api_key);
+		
+		$question = new Question($db);
+		$question->questionID = isset($_GET['id']) ? $_GET['id'] : die();
+		
+		$question->read();
+
+		if (!empty($question->questionID)) {
+			if ($question->question_type == "custom") {
+				$item = array(
+					'questionID' => $questionID,
+					'question' => $question,
+					'question_charlim' => $question_charLim,
+					'question_type' => $question_type
+				);
+
+			}else {
+				$item = array(
+					'questionID' => $questionID,
+					'question' => $question,
+					'question_type' => $question_type,
+					'choices' => array()							
+				);
+				
+				for($i = 0; $i <= count($choices); $i++) {
+					$item['data']['choices'][$i + 1] = $choice;
+				}
+			}
+
+			echo json_encode($item, JSON_PRETTY_PRINT);
+		} else {
+			echo json_encode(array('message' => 'No questions found.'));
+		}
+	} else {
+		echo json_encode(array('message' => 'Wrong HTTP request method. Use GET instead.'));
+	}
+
+?>
