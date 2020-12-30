e1fb208687e27aefb093e1d616881461f0bf470e
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2020-12-30 19:05:50 +0000

    API implemented to save the number of falls

--- app/screens/FallsPage.js
+++ app/screens/FallsPage.js
@@ -4,30 +4,37 @@ import { ScrollView, StyleSheet, Text, TextInput, View, TouchableOpacity } from 
 import { globalColors, globalStyles, globalComponentStyles } from '../styles/global';
 import { TopBar } from '../components/TopBar';
 import Card from '../components/Card';
+import LoadingScreen from '../components/LoadingScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { showMessage, hideMessage } from 'react-native-flash-message';
 
 export const FallsPage = ({ navigation }) => {
 
-	const [falls, setFalls] = React.useState();
-	const [diary, setDiary] = React.useState({});
+	const [falls, setFalls] = React.useState(0);
+	const [diary, setDiary] = React.useState();
+	const [loading, setLoading] = React.useState(false);
 
 	return (
 		<View>
+			{ loading &&
+				<LoadingScreen>Loading...</LoadingScreen>
+			}
 			<TopBar navigation={navigation}>Recording Falls</TopBar>
 			<ScrollView style={styles.cardContainer} contentContainerStyle={{paddingBottom: 20}}>
 				<Card>
 					<Text style={globalComponentStyles.cardTitle}>Today's Number of Falls</Text>
-					<TextInput style={globalComponentStyles.inputField} placeholder="Number..." multiline={false} keyboardType="numeric" onChangeText={(value) => setFalls(value)} value={falls}></TextInput>
+					<TextInput style={globalComponentStyles.inputField} placeholder="Number..." multiline={false} keyboardType="numeric" onChangeText={(value) => setFalls(value)} value={falls.toString()}></TextInput>
 					<View style={styles.buttonWrapper}>
-						<TouchableOpacity style={styles.actionButton} onPress={() => saveAnswer(1, answers[1])}>
+						<TouchableOpacity style={styles.actionButton} onPress={() => saveFalls()}>
 							<Text style={styles.actionText}>Confirm</Text>
 						</TouchableOpacity>
 					</View>
 				</Card>
 				<Card>
 					<Text style={globalComponentStyles.cardTitle}>Diary Entry</Text>
-					<TextInput style={[globalComponentStyles.inputFieldMultiline,{height: 120}]} placeholder="..." multiline={true} onChangeText={(value) => setDiary({ ...diary, 1:value })} value={diary[1]}></TextInput>
+					<TextInput style={[globalComponentStyles.inputFieldMultiline,{height: 120}]} placeholder="..." multiline={true} onChangeText={(value) => setDiary(value)} value={diary}></TextInput>
 					<View style={styles.buttonWrapper}>
-						<TouchableOpacity style={styles.actionButton} onPress={() => saveAnswer(1, answers[1])}>
+						<TouchableOpacity style={styles.actionButton} onPress={() => saveDiary()}>
 							<Text style={styles.actionText}>Save</Text>
 						</TouchableOpacity>
 					</View>
@@ -35,15 +42,49 @@ export const FallsPage = ({ navigation }) => {
 			</ScrollView>
 		</View>
 	);
-}
 
-function saveAnswer(key, value) {
-	setLoading(true);
-	setTimeout(() => {
-		setLoading(false);
-	}, 100);
-}
+	async function saveFalls() {
+		
+		setLoading(true);
 
+		setTimeout(() => {
+			setLoading(false);
+		}, 5000);
+
+		let token = await AsyncStorage.getItem("token");
+
+		let patientID = await AsyncStorage.getItem("patientID");
+
+		let endpoint = "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/falls/create.php?key=" + token;
+
+		let body = { patientID:patientID, falls:falls};
+
+		fetch(endpoint, {
+			method: "POST",
+			headers: {
+				Accept: "application/json", "Content-Type": "application/json"
+			},
+			body: JSON.stringify(body)
+		})
+		.then(() => {
+			setLoading(false);
+		})
+		.catch((error) => {
+			console.log(error);
+			setLoading(false);
+			showMessage({
+				message: "Network Error",
+				type: "danger"
+			});
+		});
+	}
+
+	async function saveDiary() {
+
+	}
+
+
+}
 
 const styles = StyleSheet.create({
 	container: {
