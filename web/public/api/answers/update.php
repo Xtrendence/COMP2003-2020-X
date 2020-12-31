90279e7e15c59beb9ca125a868be51021775da57
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2020-12-31 03:52:35 +0000

    update.php

---
+++ web/public/api/answers/update.php
@@ -0,0 +1,23 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+
+	if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+		include_once '../config/Database.php';
+		include_once '../models/Answer.php';
+
+		$api_key = isset($_GET['key']) ? $_GET['key'] : die(json_encode(array('message' => 'No API key provided.')));
+
+		$database = new Database();
+		$db = $database->connect($api_key);
+
+        $input = json_decode(file_get_contents('php://input'), true);
+        
+        $answer = new Answer($db);
+        $answer->answer = isset($input['answer']) ? $input['answer'] : die();
+
+        $answer->update();
+    } else {
+		echo json_encode(array('message' => 'Wrong HTTP request method. Use PUT instead.'));
+	}
+?>
