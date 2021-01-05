86e0775e43f4ef7b28e93357f50f4f4f4a86f2c1
JoshBent <54994720+JoshBent@users.noreply.github.com>
2021-01-05 14:33:38 +0000

    Fixes
    Fixes implemented from previous commit's comments.

--- web/public/assets/js/users.js
+++ web/public/assets/js/users.js
@@ -17,41 +17,37 @@ document.addEventListener("DOMContentLoaded", () => {
         xhr.addEventListener("readystatechange", function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 let json = xhr.responseText;
-                try {
                     let users = JSON.parse(json);
                     let keys = Object.keys(users["data"]);
                     console.log(users);
-                        try {
-                            // layer.innerHTML = "<tr><th>User ID</th><th>First Name</th><th>Last Name</th><th>Actions</th></tr>";
-                            let patient = users["data"];
-                            for (let i = 0; i < keys.length; i++){
-                                console.log("123");
-                                let userID = patient[i]["patientID"];
-                                let firstName = patient[i]["patient_fName"];
-                                let lastName = patient[i]["patient_lName"];
-                                let row = document.createElement("tr");
-                                let cell1 = row.insertCell(0);
-                                let cell2 = row.insertCell(1);
-                                let cell3 = row.insertCell(2);
-                                let cellAct = row.insertCell(3);
-                                row.id = userID;
-                                cell1.innerHTML = (userID);
-                                cell2.innerHTML = (firstName);
-                                cell3.innerHTML = (lastName);
-                                layer.appendChild(row);
-                                layer.appendChild(cell1);
-                                layer.appendChild(cell2);
-                                layer.appendChild(cell3);
+                    try {
+                        layer.innerHTML = "";
+                        let patient = users["data"];
+                        for (let i = 0; i < keys.length; i++){
+                            console.log("123");
+                            let userID = patient[i]["patientID"];
+                            let firstName = patient[i]["patient_fName"];
+                            let lastName = patient[i]["patient_lName"];
+                            let row = document.createElement("tr");
+                            let cell1 = row.insertCell(0);
+                            let cell2 = row.insertCell(1);
+                            let cell3 = row.insertCell(2);
+                            let cellAct = row.insertCell(3);
+                            row.id = userID;
+                            cell1.innerHTML = (userID);
+                            cell2.innerHTML = (firstName);
+                            cell3.innerHTML = (lastName);
+                            layer.appendChild(row);
+                            layer.appendChild(cell1);
+                            layer.appendChild(cell2);
+                            layer.appendChild(cell3);
 
-                                createButtons(deleteButton, editButton, profileButton, answerButton, questButton, userID, cellAct, row);
-                            }
+                            createButtons(deleteButton, editButton, profileButton, answerButton, questButton, userID, cellAct, row);
+                        }
                     } catch {
-                        console.error("error123123");
+                        console.error("error");
                     }
 
-                } catch {
-                    console.error("errorerrorerror");
-                }
             }
         });
         xhr.open("GET", "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/users/read-range.php?from=" + from + "&to=" + to + "&key=8c068d98-874e-46ab-b2a1-5a5eb45a40a6", true);
