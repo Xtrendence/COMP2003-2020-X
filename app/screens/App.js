b9d9f9800f11694c124ac45e3c80a5c305094bf0
Xtrendence <xtrendence@gmail.com>
2021-01-16 19:59:15 +0000

    Android's navigation bar color is now changed to match the app's theme.

--- app/screens/App.js
+++ app/screens/App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import { StackNavigator } from '../components/StackNavigator';
 import Notifier from '../utils/Notifier';
+import { globalColors } from '../styles/global';
+
+changeNavigationBarColor(rgbToHex(globalColors.accentDark), false);
 
 export default class App extends Component {
 	constructor(props) {
@@ -27,4 +31,13 @@ export default class App extends Component {
 			notifier.localNotification(notification.title, notification.message);
 		}
 	}
+}
+
+function rgbToHex(rgb) {
+	let numbers = rgb.split("(")[1].split(")")[0].split(",");
+	let hexArray = numbers.map((number) => {
+		number = parseInt(number).toString(16);
+		return (number.length === 1) ? "0" + number : number;
+	});
+	return "#" + hexArray.join("");
 }
