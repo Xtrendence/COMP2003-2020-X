5f0fa01b49dab43e371d30daa69bed036b9d4d9d
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-03-04 18:05:07 +0000

    Have started to create if statement inside event listener.
    Have started to create if statement inside event listener to start pulling in the data field values for their corresponding small card containers. Using a XML HTTP request, a XHR .open element was used containing the relevant "read" api endpoint. This is so the users info can read in using the GET request .

--- web/public/assets/js/view-user.js
+++ web/public/assets/js/view-user.js
@@ -1,14 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+/* variables for getting api URL data */
+
     let url = new URL(window.location.href);
     let patID = url.searchParams.get("id");
 
     let titleCard = document.getElementById("user-title");
-    let title = "User Profile - ";
+    let title = "User Profile - User ";
     let addID = title.concat(patID);
     titleCard.innerText = addID;
 
+    let content = document.getElementById("content")
+    let xhr = new XMLHttpRequest();
 
+    let refCard = document.getElementById("NHS-RN");
+
+
+    xhr.addEventListener("readystatechange", function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            let json = xhr.responseText;
+            let users = JSON.parse(json);
+            let keys = Object.keys(users);
+
+            try {
+                let patient = users;
+                for (let i = 0; i < keys.length; i++){
+                    let ref = patient["patient_nhsRef"];
+                    let userName = patient["patient_username"];
+                    let firstName = patient["patient_fName"];
+                    let lastName = patient["patient_lName"];
+
+                    refCard.innerText = ref;
+                }
+            } catch {
+                console.error("error");
+            }
+
+        }
+    });
+    xhr.open("GET", "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/users/read.php?key=8c068d98-874e-46ab-b2a1-5a5eb45a40a6&id=" + patID, true);
+    xhr.send();
 
 
 });
