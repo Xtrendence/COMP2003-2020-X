b9eb10cb8565d411631f0d71ab87d66f647e0546
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-03-05 11:41:55 +0000

    Have finished adding document.getElementByIds for user fields in JS file.
    Have finished adding document.getElementById's with JS variables for api data fields to be pulled in from the relevant endpoint for the user being currently viewed and then placed into the corresponding span tag within each small card. The variables outside of the if statement get the span ids to then be assigned to the card variable name. Within the if statement, another 12 variables are created which equal to their relevant api fields from the "read" endpoint. Finally, the 12 card variables then equal to the 12 endpoint variables and use ".innerText" to place the data value of what that field is into the HTML span on the web page. Will code comment and tidy up code.

--- web/public/assets/js/view-user.js
+++ web/public/assets/js/view-user.js
@@ -12,10 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let content = document.getElementById("content")
     let xhr = new XMLHttpRequest();
-
+/* */
     let refCard = document.getElementById("NHS-RN");
     let usernameCard = document.getElementById("username");
     let fnCard = document.getElementById("firstname");
+    let lnCard = document.getElementById("lastname");
+    let dobCard = document.getElementById("DOB");
+    let aline1Card = document.getElementById("AL1");
+    let aline2Card = document.getElementById("AL2");
+    let postcodeCard = document.getElementById("postcode");
+    let tnCard = document.getElementById("TN");
+    let mnCard = document.getElementById("MN");
+    let emailCard = document.getElementById("email");
+    let commentsCard = document.getElementById("comments");
 
 
     xhr.addEventListener("readystatechange", function() {
@@ -30,13 +39,31 @@ document.addEventListener("DOMContentLoaded", () => {
                     let ref = patient["patient_nhsRef"];
 
                     let userName = patient["patient_username"];
-
                     let firstName = patient["patient_fName"];
                     let lastName = patient["patient_lName"];
+                    let dateBirth = patient["patient_dob"];
+                    let addressLine1 = patient["patient_addressI"];
+                    let addressLine2 = patient["patient_addressII"];
+                    let postCode = patient["patient_postcode"];
+                    let telephoneNumber = patient["patient_tel"];
+                    let mobileNumber = patient["patient_mobile"];
+                    let emailAddress = patient["patient_email"];
+                    let comments = patient["patient_comment"];
 
                     refCard.innerText = ref;
                     usernameCard.innerText = userName;
                     fnCard.innerText = firstName
+                    lnCard.innerText = lastName;
+                    dobCard.innerText = dateBirth;
+                    aline1Card.innerText = addressLine1;
+                    aline2Card.innerText = addressLine2;
+                    postcodeCard.innerText = postCode;
+                    tnCard.innerText = telephoneNumber;
+                    mnCard.innerText = mobileNumber;
+                    emailCard.innerText = emailAddress;
+                    commentsCard.innerText = comments;
+
+
 
 
                 }
