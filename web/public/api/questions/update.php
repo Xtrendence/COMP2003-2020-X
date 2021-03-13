c71c9dfb5a47c29a1ee56f0408663bc609b71236
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-03-13 16:03:16 +0000

    question update
    avoided using die() by instead using an array, needs to have stored procedure used to call

--- web/public/api/questions/update.php
+++ web/public/api/questions/update.php
@@ -8,23 +8,31 @@
 
 		$api_key = isset($_GET['key']) ? $_GET['key'] : die(json_encode(array('message' => 'No API key provided.')));
 
-		$database = new Database();
+        $missing = [];
+
+		$database = new Database(false);
 		$db = $database->connect($api_key);
 
         $input = json_decode(file_get_contents('php://input'), true);
         
         $question = new Question($db);
-        $question->questionID = isset($input['questionID']) ? $input['questionID'] : die();
-        $question->question = isset($input['question']) ? $input['question'] : die();
-        $question->question_type = isset($input['question_type']) ? $input['question_type'] : die();
+        $question->questionID = isset($input['questionID']) ? $input['questionID'] : array_push($missing, 'questionID');
+        $question->question = isset($input['question']) ? $input['question'] : array_push($missing, 'question');
+        $question->question_type = isset($input['question_type']) ? $input['question_type'] : array_push($missing, 'question_type');
         
         if ($question->question_type == 'custom') {
-            $question->question_charLim = isset($input['question_charLim']) ? $input['question_charLim'] : die();
+            $expected = ['questionID', 'question', 'question_type', 'question_charLim'];
+            $question->question_charLim = isset($input['question_charLim']) ? $input['question_charLim'] : array_push($missing, 'question_charLim');
         } else {
-            $question->choices = isset($input['choices']) ? $input['choices'] : die();
+            $expected = ['questionID', 'question', 'question_type', 'choices'];
+            $question->choices = isset($input['choices']) ? $input['choices'] : array_push($missing, 'choices');
         }
 
-        $answer->update();
+        if (empty($missing)){
+            $answer->update();
+        } else {
+            die(json_encode(array('expected' => $expected, 'missing' => $missing), JSON_PRETTY_PRINT));
+        }
     } else {
 		echo json_encode(array('message' => 'Wrong HTTP request method. Use PUT instead.'));
 	}
