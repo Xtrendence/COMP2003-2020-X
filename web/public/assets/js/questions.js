c300d4e3bbebd05c08c32331bb224c015d7603bc
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-03-17 15:27:56 +0000

    Have started pull in data values for question wide cards.
    Within the "questions.js" file, I have created another function that will get the required question fields from the api end point named "read-all" using an XMLHttpRequest. For loop being used to retrieve the data fields and create the wide cards dynamically on the web page.  progress is still being made.

--- web/public/assets/js/questions.js
+++ web/public/assets/js/questions.js
@@ -3,33 +3,76 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("123");
     let wideCard;
     let cardWrap = document.getElementById("cards");
+    const xhr = new XMLHttpRequest();
 
 
-    function questionCard(wideCard){
+
+
+    function getQuestions() {
+        xhr.addEventListener("readystatechange", function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                let json = xhr.responseText;
+                let users = JSON.parse(json);
+                let keys = Object.keys(users["data"]);
+
+                try {
+
+                    let question = users["data"];
+                    for (let i = 0; i < keys.length; i++) {
+
+
+                        /* Variables used for "read" api endpoint field values */
+                        let questionID = question[i]["questionID"];
+                        console.log(i);
+                        let q = question[i]["question"];
+                        let questionLim = question[i]["question_charLim"];
+                        let questionType = question[i]["question_type"];
+                        let choices = question[i]["choices"];
+
+                        console.log("debug");
+                        questionCard(wideCard, questionID, q, questionLim, questionType, choices);
+                    }
+
+                } catch {
+                    console.log("error");
+                }
+
+            }
+
+
+        });
+
+        console.log("123");
+        xhr.open("GET", "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/users/read-all.php?&key=8c068d98-874e-46ab-b2a1-5a5eb45a40a6", true);
+        xhr.send();
+
+    }
+
+
+
+
+    function questionCard(wideCard, questionID, q, questionLim, questionType, choices){
 
         let cardAnchor = document.createElement("a");
 
         wideCard = document.createElement("div");
         wideCard.classList.add("wide-card");
         wideCard.id = "wideCard";
-        wideCard.innerHTML = ' <div class="text-margin">\n' +
-            '            <span class="card-heading">Patient ID:</span>\n' +
+        wideCard.innerHTML = '\n' +
+            '        <div class="text-margin">\n' +
+            '            <span class="card-heading">Question ID: '+ questionID +' </span>\n' +
             '        </div>\n' +
             '\n' +
             '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Question ID:</span>\n' +
+            '            <span class="card-heading">Question: '+ q +'</span>\n' +
             '        </div>\n' +
             '\n' +
             '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Question:</span>\n' +
+            '            <span class="card-heading">Character Limit: '+ questionLim +'</span>\n' +
             '        </div>\n' +
             '\n' +
             '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Character Limit:</span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Type:</span>\n' +
+            '            <span class="card-heading">Type: '+ questionType +' </span>\n' +
             '        </div>\n' +
             '\n' +
             '    </div>'
@@ -37,8 +80,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         cardAnchor.appendChild(wideCard);
         cardWrap.appendChild(cardAnchor);
+
     }
 
-    questionCard(wideCard);
+    getQuestions();
 
 });
