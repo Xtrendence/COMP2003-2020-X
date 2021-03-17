766b9f94df9ce9965f6cf07ff819803468751b83
Xtrendence <xtrendence@gmail.com>
2021-03-17 22:39:21 +0000

    The testing page now has a functioning login system.

--- web/public/assets/js/api-testing.js
+++ web/public/assets/js/api-testing.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	let apiKey;
 	let apiURL;
 
+	let timeoutLimit = 2000;
+
 	let inputUserUsername = document.getElementById("user-username");
 	let inputUserPassword = document.getElementById("user-password");
 
@@ -16,13 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	let divOutput = document.getElementById("output");
 
-	buttonTest.addEventListener("click", () => {
+	buttonTest.addEventListener("click", async () => {
 		apiURL = inputURL.value;
 		if (apiURL.substr(apiURL.length - 1) !== "/") {
 			apiURL = apiURL + "/";
 		}
 
-		if (empty(inputUserUsername) || empty(inputUserPassword) || empty(inputAdminUsername) || empty(inputAdminPassword)) {
+		if ((empty(inputUserUsername.value) && empty(inputUserPassword.value)) || (empty(inputAdminUsername.value) && empty(inputAdminPassword.value))) {
 			let url = new URL(window.location.href);
 			let key = url.searchParams.get("key");
 			if (empty(key)) {
@@ -31,9 +33,103 @@ document.addEventListener("DOMContentLoaded", () => {
 				apiKey = key;
 			}
 		}
+		
+		if ((!empty(inputUserUsername.value) && !empty(inputAdminUsername.value)) || (!empty(inputUserPassword.value) && !empty(inputAdminPassword.value))) {
+			Notify.error({
+				title: "Error",
+				description: "The API can't be tested as an admin and user at the same time."
+			});
+		}
+
+		if ((!empty(inputUserUsername.value) && !empty(inputUserPassword.value)) && (empty(inputAdminUsername.value) && empty(inputAdminPassword.value))) {
+			await loginUser(inputUserUsername.value, inputUserPassword.value).catch(e => {
+				Notify.error({
+					title: "Error",
+					description: e
+				});
+			});
+		}
+
+		if ((empty(inputUserUsername.value) && empty(inputUserPassword.value)) && (!empty(inputAdminUsername.value) && !empty(inputAdminPassword.value))) {
+			await loginAdmin(inputAdminUsername.value, inputAdminPassword.value).catch(e => {
+				Notify.error({
+					title: "Error",
+					description: e
+				});
+			});
+		}
 
 		attemptStart();
 	});
+
+	function loginUser(username, password) {
+		return new Promise((resolve, reject) => {
+			let xhr = new XMLHttpRequest();
+			
+			xhr.addEventListener("readystatechange", () => {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					console.log(xhr.responseText);
+					if (validJSON(xhr.responseText)) {
+						let response = JSON.parse(xhr.responseText);
+
+						if (response.valid) {
+							apiKey = response.token;
+
+							console.log(apiKey);
+
+							Notify.success({
+								title: "Success",
+								description: "You are now logged in as a user."
+							});
+
+							resolve();
+						} else {
+							reject("Login failed.");
+						}
+					} else {
+						reject("Invalid JSON.");
+					}
+				}
+			});
+
+			xhr.open("POST", apiURL + "users/login.php", true);
+			xhr.send(JSON.stringify({ patient_username:username, patient_password:password, fcmToken:"-" }));
+		});
+	}
+
+	function loginAdmin(username, password) {
+		return new Promise((resolve, reject) => {
+			let xhr = new XMLHttpRequest();
+			
+			xhr.addEventListener("readystatechange", () => {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (validJSON(xhr.responseText)) {
+						let response = JSON.parse(xhr.responseText);
+
+						if (response.valid) {
+							apiKey = response.token;
+
+							console.log(apiKey);
+
+							Notify.success({
+								title: "Success",
+								description: "You are now logged in as an admin."
+							});
+
+							resolve();
+						} else {
+							reject("Login failed.");
+						}
+					} else {
+						reject("Invalid JSON.");
+					}
+				}
+			});
+
+			xhr.open("POST", apiURL + "admins/login.php", true);
+			xhr.send(JSON.stringify({ researcher_username:username, researcher_password:password }));
+		});
+	}
 
 	function attemptStart() {
 		if (empty(apiKey)) {
@@ -47,282 +143,462 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	async function runTests() {
-		divOutput.innerHTML = "";
+		try {
+			divOutput.innerHTML = "";
 
-		log("info", "Testing /admins/");
+			log("info", "Testing /admins/");
 
-		await adminsCreate();
-		await adminsDelete();
-		await adminsLogin();
-		await adminsLogout();
-		await adminsReadAll();
-		await adminsReadRange();
-		await adminRead();
-		await adminUpdate();
+			await adminsCreate().catch(e => {
+				handleError(e);
+			});
+			await adminsDelete().catch(e => {
+				handleError(e);
+			});
+			await adminsLogin().catch(e => {
+				handleError(e);
+			});
+			await adminsLogout().catch(e => {
+				handleError(e);
+			});
+			await adminsReadAll().catch(e => {
+				handleError(e);
+			});
+			await adminsReadRange().catch(e => {
+				handleError(e);
+			});
+			await adminRead().catch(e => {
+				handleError(e);
+			});
+			await adminUpdate().catch(e => {
+				handleError(e);
+			});
 
-		log("info", "Testing /answers/");
+			log("info", "Testing /answers/");
 
-		await answersCreate();
-		await answersDelete();
-		await answersReadAll();
-		await answersReadUser();
-		await answersRead();
-		await answersUpdate();
+			await answersCreate().catch(e => {
+				handleError(e);
+			});
+			await answersDelete().catch(e => {
+				handleError(e);
+			});
+			await answersReadAll().catch(e => {
+				handleError(e);
+			});
+			await answersReadUser().catch(e => {
+				handleError(e);
+			});
+			await answersRead().catch(e => {
+				handleError(e);
+			});
+			await answersUpdate().catch(e => {
+				handleError(e);
+			});
 
-		log("info", "Testing /diary-entries/");
+			log("info", "Testing /diary-entries/");
 
-		await diaryEntriesCreate();
-		await diaryEntriesDelete();
-		await diaryEntriesReadAll();
-		await diaryEntriesReadDate();
-		await diaryEntriesReadUser();
-		await diaryEntriesRead();
-		await diaryEntriesUpdate();
+			await diaryEntriesCreate().catch(e => {
+				handleError(e);
+			});
+			await diaryEntriesDelete().catch(e => {
+				handleError(e);
+			});
+			await diaryEntriesReadAll().catch(e => {
+				handleError(e);
+			});
+			await diaryEntriesReadDate().catch(e => {
+				handleError(e);
+			});
+			await diaryEntriesReadUser().catch(e => {
+				handleError(e);
+			});
+			await diaryEntriesRead().catch(e => {
+				handleError(e);
+			});
+			await diaryEntriesUpdate().catch(e => {
+				handleError(e);
+			});
 
-		log("info", "Testing /falls/");
+			log("info", "Testing /falls/");
 
-		await fallsCreate();
-		await fallsDelete();
-		await fallsExport();
-		await fallsReadAll();
-		await fallsReadDate();
-		await fallsReadUser();
-		await fallsRead();
+			await fallsCreate().catch(e => {
+				handleError(e);
+			});
+			await fallsDelete().catch(e => {
+				handleError(e);
+			});
+			await fallsExport().catch(e => {
+				handleError(e);
+			});
+			await fallsReadAll().catch(e => {
+				handleError(e);
+			});
+			await fallsReadDate().catch(e => {
+				handleError(e);
+			});
+			await fallsReadUser().catch(e => {
+				handleError(e);
+			});
+			await fallsRead().catch(e => {
+				handleError(e);
+			});
 
-		log("info", "Testing /questions/");
+			log("info", "Testing /questions/");
 
-		await questionsCreate();
-		await questionsDelete();
-		await questionsReadAll();
-		await questionsReadRange();
-		await questionsRead();
-		await questionsUpdate();
+			await questionsCreate().catch(e => {
+				handleError(e);
+			});
+			await questionsDelete().catch(e => {
+				handleError(e);
+			});
+			await questionsReadAll().catch(e => {
+				handleError(e);
+			});
+			await questionsReadRange().catch(e => {
+				handleError(e);
+			});
+			await questionsRead().catch(e => {
+				handleError(e);
+			});
+			await questionsUpdate().catch(e => {
+				handleError(e);
+			});
 
-		log("info", "Testing /users/");
+			log("info", "Testing /users/");
 
-		await usersCreate();
-		await usersDelete();
-		await usersLogin();
-		await usersLogout();
-		await usersReadAll();
-		await usersReadRange();
-		await usersRead();
-		await usersUpdate();
+			await usersCreate().catch(e => {
+				handleError(e);
+			});
+			await usersDelete().catch(e => {
+				handleError(e);
+			});
+			await usersLogin().catch(e => {
+				handleError(e);
+			});
+			await usersLogout().catch(e => {
+				handleError(e);
+			});
+			await usersReadAll().catch(e => {
+				handleError(e);
+			});
+			await usersReadRange().catch(e => {
+				handleError(e);
+			});
+			await usersRead().catch(e => {
+				handleError(e);
+			});
+			await usersUpdate().catch(e => {
+				handleError(e);
+			});
+		} catch(e) {
+			console.log(e);
+		}
 	}
 
 	function adminsCreate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminsDelete() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminsLogin() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminsLogout() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminsReadAll() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminsReadRange() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminRead() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function adminUpdate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 
 	function answersCreate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function answersDelete() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function answersReadAll() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function answersReadUser() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function answersRead() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function answersUpdate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 
 	function diaryEntriesCreate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function diaryEntriesDelete() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function diaryEntriesReadAll() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function diaryEntriesReadDate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function diaryEntriesReadUser() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function diaryEntriesRead() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function diaryEntriesUpdate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 
 	function fallsCreate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function fallsDelete() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function fallsExport() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function fallsReadAll() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function fallsReadDate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function fallsReadUser() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function fallsRead() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 
 	function questionsCreate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function questionsDelete() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function questionsReadAll() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function questionsReadRange() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function questionsRead() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function questionsUpdate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 
 	function usersCreate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersDelete() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersLogin() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersLogout() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersReadAll() {
 		return new Promise((resolve, reject) => {
+			sendRequest("GET", apiURL + "users/read-all.php?key=" + apiKey).then((result) => {
+				handleResponse(result.endpoint, result.response);
+				resolve();
+			}).catch((e) => {
+				handleError("Error - " + result.endpoint);
+				reject(e);
+			});
 
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersReadRange() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersRead() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 	function usersUpdate() {
 		return new Promise((resolve, reject) => {
-
+			setTimeout(() => {
+				reject("Timeout - " + arguments.callee.name + "()");
+			}, timeoutLimit);
 		});
 	}
 
@@ -366,7 +642,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	function handleError(error) {
-
+		log("error", error);
 	}
 
 	function generateID() {
