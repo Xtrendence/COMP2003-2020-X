6c5028fb4ea750854f747e62c869bb36503cce7e
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-03-26 17:32:05 +0000

    Have worked on "choiceCard" function.
    Have worked on "choiceCard" function within JS. Existing written type questions and choice questions will dynamically appear within their own wide cards. However, not all choice types will appear from the lists array within any choice type question. Working on a solution to make the array of choice lists appear.

--- web/public/assets/js/questions.js
+++ web/public/assets/js/questions.js
@@ -6,13 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const xhr = new XMLHttpRequest();
 
 
-
+    console.log("debug");
 
     function getQuestions() {
         xhr.addEventListener("readystatechange", function () {
+            console.log("hello");
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 let json = xhr.responseText;
                 let users = JSON.parse(json);
+                console.log("hello 2");
                 let keys = Object.keys(users["data"]);
 
                 try {
@@ -24,28 +26,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
                         /* Variables used for "read" api endpoint field values */
                         let questionID = question[i]["questionID"];
-                        console.log(questionID + "hello");
                         let q = question[i]["question"];
                         let questionLim = question[i]["question_charLim"];
                         let questionType = question[i]["question_type"];
                         let choices = question[i]["choices"];
-                        let choicesLength = choices.length;
-
-                        console.log(choicesLength);
 
                         console.log("debug");
 
+
+
+                        console.log("debug");
+
+                        /*questionCard(wideCard, questionID, q, questionLim, questionType);
+                        choiceCard(wideCard, questionID, q, questionLim, questionType, choices);*/
+
                         if (questionType == "choice") {
+
 
                             choiceCard(wideCard, questionID, q, questionLim, questionType, choices);
 
                         } else {
                             questionCard(wideCard, questionID, q, questionLim, questionType);
                         }
+
                     }
 
                 } catch {
                     console.log("error");
+
+
                 }
 
             }
@@ -94,39 +103,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
+
     getQuestions();
 
 
     function choiceCard(wideCard, questionID, q, questionLim, questionType, choices) {
 
-        let cardAnchor = document.createElement("a");
+        try {
+            let cardAnchor = document.createElement("a");
+
+            let printout = Object.keys(choices).length;
 
 
-        wideCard = document.createElement("div");
-        wideCard.classList.add("wide-card");
-        wideCard.id = "wideCard";
-        wideCard.innerHTML = '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Question ID:'+ questionID +' </span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Question: '+ q +'</span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Type: '+ questionType +'</span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Choices: '+ choices[1] +' </span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '    </div>'
+            wideCard = document.createElement("div");
+            wideCard.classList.add("wide-card");
+            wideCard.id = "wideCard";
 
 
-        cardAnchor.appendChild(wideCard);
-        cardWrap.appendChild(cardAnchor);
+
+                wideCard.innerHTML = '\n' +
+                    '        <div class="text-margin">\n' +
+                    '            <span class="card-heading">Question ID:' + questionID + ' </span>\n' +
+                    '        </div>\n' +
+                    '\n' +
+                    '        <div class="text-margin">\n' +
+                    '            <span class="card-heading">Question: ' + q + '</span>\n' +
+                    '        </div>\n' +
+                    '\n' +
+                    '        <div class="text-margin">\n' +
+                    '            <span class="card-heading">Type: ' + questionType + '</span>\n' +
+                    '        </div>\n' +
+                    '\n' +
+                    '        <div class="text-margin">\n' +
+                    '            <span class="card-heading">Choices: '
+                    for (let i=0; i < Object.keys(choices).length; i++) {
+                        + choices[i]
+                    }
+
+
+                    + ' </span>\n' +
+                    '        </div>\n' +
+                    '\n' +
+                    '    </div>'
+
+                cardAnchor.appendChild(wideCard);
+                cardWrap.appendChild(cardAnchor);
+
+
+
+
+
+        } catch {
+            console.log("error");
+        }
 
 
     }
