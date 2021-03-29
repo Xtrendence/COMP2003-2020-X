423b009891c86e8854eb4bce7eb6d4e6e74c9fcf
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-03-29 12:20:56 +0100

    session token removed from local storage

--- web/public/assets/js/settings.js
+++ web/public/assets/js/settings.js
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
     toggleContainer.addEventListener('click', function(){
         if (document.body.hasAttribute('data-theme','dark')){
             document.body.removeAttribute('data-theme', 'dark');
-            togglePosition.classList.remove("toggle-button-active")
-            localStorage.removeItem('theme', 'dark')
+            togglePosition.classList.remove("toggle-button-active");
+            localStorage.removeItem('theme', 'dark');
         } else {
             document.body.setAttribute('data-theme', 'dark');
-            togglePosition.classList.add("toggle-button-active")
-            localStorage.setItem('theme', 'dark')
+            togglePosition.classList.add("toggle-button-active");
+            localStorage.setItem('theme', 'dark');
         }
     });
 
@@ -30,12 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 try {
                     let response = JSON.parse(responseJSON);
+                    localStorage.removeItem('sessionToken', sessionToken);
                 } catch(e) {
                     console.log(e);
                 }
             }
         });
-        
+
     });
 
     if(localStorage.getItem('theme') === 'dark'){
