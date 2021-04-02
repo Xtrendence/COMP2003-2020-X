c51e1d9ed715e17d23dea1f35c1050998317028a
bethmward <bethany.m.ward@students.plymouth.ac.uk>
2021-04-02 13:16:58 +0100

    Removed patient login and navbar
    *Now only admin login code is present
    *Moved around JS code to be in chronological order
    *Also adjusted style.css and added actual apiURL to login.js

--- web/public/assets/js/login.js
+++ web/public/assets/js/login.js
@@ -2,50 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const Notify = new XNotify("BottomRight");
 
     let apiKey;
-    let apiURL;
+    let apiURL = "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/";
     let apiAccess;
-    let timeoutLimit = 12500;
-    let requestInterval = 750;
-
-    let inputPatientUsername = document.getElementById("patient-username");
-    let inputPatientPassword = document.getElementById("patient-password");
 
     let inputAdminUsername = document.getElementById("admin-username");
     let inputAdminPassword = document.getElementById("admin-password");
 
-    let buttonPatientLogin = document.getElementById("patient-login");
     let buttonAdminLogin = document.getElementById("admin-login");
-
-    buttonPatientLogin.addEventListener("click", async () => {
-        if (!buttonPatientLogin.classList.contains("disabled")) {
-            if (((empty(inputPatientPassword.value)) && (empty(inputPatientUsername.value)))) {
-                let url = new URL(window.location.href);
-                let key = url.searchParams.get("key");
-                if (empty(key)) {
-                    apiKey = empty(inputKey.value) ? window.prompt("Login failed. Do you have an API key already?") : inputKey.value;
-                } else {
-                    apiKey = key;
-                }
-            }
-
-            if (((empty(inputPatientPassword.value)) && (!empty(inputPatientUsername.value))) || ((!empty(inputPatientPassword)) && (empty(inputPatientUsername)))) {
-                Notify.error({
-                    title: "Error",
-                    description: "Patient login details must be entered."
-                });
-            }
-
-            if ((!empty(inputPatientUsername.value)) && (!empty(inputPatientPassword.value))) {
-                await loginPatient(inputPatientUsername.value, inputPatientPassword.value).catch(e => {
-                    Notify.error({
-                        title: "Error",
-                        description: e
-                    });
-                });
-                attemptStart();
-            }
-        }
-    });
 
     buttonAdminLogin.addEventListener("click", async () => {
         if (!buttonAdminLogin.classList.contains("disabled")) {
@@ -53,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let url = new URL(window.location.href);
                 let key = url.searchParams.get("key");
                 if (empty(key)) {
-                    apiKey = empty(inputKey.value) ? window.prompt("Login failed. Do you have an API key already?") : inputKey.value;
+                    apiKey = empty(inputKey.value) ? window.prompt("Empty API key...") : inputKey.value;
                 } else {
                     apiKey = key;
                 }
@@ -77,71 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
-
-    function attemptStart() {
-        if (empty(apiKey)) {
-            Notify.error({
-                title: "Error",
-                description: "Cannot log in without an API key."
-            });
-        } else {
-            login();
-        }
-    }
-
-    async function login() {
-        console.log("Logging in...");
-        buttonPatientLogin.classList.add("disabled");
-        buttonAdminLogin.classList.add("disabled");
-
-        if (apiKey.includes("patient") || apiKey.includes("user")) {
-            log("alert", "Using patient token");
-            apiAccess = "user";
-
-        } else if (apiKey.includes("admin") || apiKey.includes("researcher")) {
-            log("alert", "Using admin token");
-            apiAccess = "admin";
-        } else {
-            log("alert", "Using development token");
-            apiAccess = "admin";
-        }
-    }
-
-    function loginPatient(username, password) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-
-            xhr.addEventListener("readystatechange", () => {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (validJSON(xhr.responseText)) {
-                        let response = JSON.parse(xhr.responseText);
-
-                        if (response.valid) {
-                            apiKey = response.token;
-
-                            console.log(apiKey);
-
-                            Notify.success({
-                                title: "Success",
-                                description: "You are now logged in as a patient."
-                            });
-
-                            log("success", "Logged In As Patient");
-
-                            resolve();
-                        } else {
-                            reject("Login failed.");
-                        }
-                    } else {
-                        reject("Invalid JSON.");
-                    }
-                }
-            });
-
-            xhr.open("POST", apiURL + "users/login.php", true);
-            xhr.send(JSON.stringify({ patient_username: username, patient_password: password, fcmToken: "-" }));
-        });
-    }
 
     function loginAdmin(username, password) {
         return new Promise((resolve, reject) => {
@@ -188,7 +86,31 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         catch (e) { }
         return false;
+    }
+
+    function attemptStart() {
+        if (empty(apiKey)) {
+            Notify.error({
+                title: "Error",
+                description: "Cannot log in without an API key."
+            });
+        } else {
+            login();
         }
+    }
+
+    async function login() {
+        console.log("Logging in...");
+        buttonAdminLogin.classList.add("disabled");
+
+        if (apiKey.includes("admin") || apiKey.includes("researcher")) {
+            log("alert", "Using admin token");
+            apiAccess = "admin";
+        } else {
+            log("alert", "Using development token");
+            apiAccess = "admin";
+        }
+    }
 
     function log(className, text) {
         let element = document.createElement("span");
@@ -206,5 +128,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         return false;
     }
-
  });
