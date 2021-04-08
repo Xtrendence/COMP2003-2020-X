9435c7d51e5cbe69de7a1572105e96540d7fa7f1
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-04-08 20:29:43 +0100

    comments added to js

--- web/public/assets/js/settings.js
+++ web/public/assets/js/settings.js
@@ -7,6 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
         let logout = document.getElementById('logout');
         let sessionToken = localStorage.getItem("sessionToken");
 
+        /**
+         * @desc when toggle is clicked, the page will switch between a light mode and dark mode
+         * adds and removes the theme appropriatly from local storage
+         */
         toggleContainer.addEventListener('click', function(){
             if (document.body.hasAttribute('data-theme','dark')){
                 document.body.removeAttribute('data-theme', 'dark');
@@ -19,10 +23,13 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
 
+        /**
+         * @desc logout button calls the logout api then returns user to login page.
+         */
         logout.addEventListener('click', function(){
             let xhr = new XMLHttpRequest();
 
-            xhr.open("POST", "../../api/admins/logout.php?key=" + sessionToken+ "", true);
+            xhr.open("POST", "../../api/admins/logout.php?key=" + sessionToken + "", true);
             xhr.send(JSON.stringify(body));
 
             xhr.addEventListener("readystatechange", function() {
@@ -40,6 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
 
+        /**
+        * @desc on DOM loaded, it checks to see if localStorage has the key:'theme', and if it does is it's value:'dark'.
+        *      when that is true, it sets the body with an attribute to turn the theme dark.
+        */
         if(localStorage.getItem('theme') === 'dark'){
             document.body.setAttribute('data-theme', 'dark');
             togglePosition.classList.add("toggle-button-active")
