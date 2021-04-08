08465c821a4a997a90ced2b32835097a369d6529
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-04-08 20:11:01 +0100

    adding function for session tokens

--- web/public/assets/js/settings.js
+++ web/public/assets/js/settings.js
@@ -1,51 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
+    verifySession(localStorage.getItem("sessionToken")).then(result => {
 
-    let togglePosition = document.getElementById("toggleBut");
-    let toggleContainer = document.getElementById('toggle');
+        let togglePosition = document.getElementById("toggleBut");
+        let toggleContainer = document.getElementById('toggle');
 
-    let logout = document.getElementById('logout');
-    let sessionToken = localStorage.getItem("sessionToken");
+        let logout = document.getElementById('logout');
+        let sessionToken = localStorage.getItem("sessionToken");
 
-    toggleContainer.addEventListener('click', function(){
-        if (document.body.hasAttribute('data-theme','dark')){
-            document.body.removeAttribute('data-theme', 'dark');
-            togglePosition.classList.remove("toggle-button-active");
-            localStorage.removeItem('theme', 'dark');
-        } else {
-            document.body.setAttribute('data-theme', 'dark');
-            togglePosition.classList.add("toggle-button-active");
-            localStorage.setItem('theme', 'dark');
-        }
-    });
-
-    logout.addEventListener('click', function(){
-        let xhr = new XMLHttpRequest();
-
-        xhr.open("POST", "../../api/admins/logout.php?key=" + sessionToken+ "", true);
-        xhr.send(JSON.stringify(body));
-
-        xhr.addEventListener("readystatechange", function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                let responseJSON = xhr.responseText;
-
-                try {
-                    let response = JSON.parse(responseJSON);
-                    localStorage.removeItem('sessionToken', sessionToken);
-                } catch(e) {
-                    console.log(e);
-                }
+        toggleContainer.addEventListener('click', function(){
+            if (document.body.hasAttribute('data-theme','dark')){
+                document.body.removeAttribute('data-theme', 'dark');
+                togglePosition.classList.remove("toggle-button-active");
+                localStorage.removeItem('theme', 'dark');
+            } else {
+                document.body.setAttribute('data-theme', 'dark');
+                togglePosition.classList.add("toggle-button-active");
+                localStorage.setItem('theme', 'dark');
             }
         });
 
+        logout.addEventListener('click', function(){
+            let xhr = new XMLHttpRequest();
+
+            xhr.open("POST", "../../api/admins/logout.php?key=" + sessionToken+ "", true);
+            xhr.send(JSON.stringify(body));
+
+            xhr.addEventListener("readystatechange", function() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    let responseJSON = xhr.responseText;
+
+                    try {
+                        let response = JSON.parse(responseJSON);
+                        localStorage.removeItem('sessionToken', sessionToken);
+                    } catch(e) {
+                        console.log(e);
+                    }
+                }
+            });
+
+        });
+
+        if(localStorage.getItem('theme') === 'dark'){
+            document.body.setAttribute('data-theme', 'dark');
+            togglePosition.classList.add("toggle-button-active")
+        } else {
+            document.body.removeAttribute('data-theme', 'dark');
+            togglePosition.classList.remove("toggle-button-active")
+        }
+    }).catch(error => {
+        window.location.replace("./login.php");
     });
-
-    if(localStorage.getItem('theme') === 'dark'){
-        document.body.setAttribute('data-theme', 'dark');
-        togglePosition.classList.add("toggle-button-active")
-    } else {
-        document.body.removeAttribute('data-theme', 'dark');
-        togglePosition.classList.remove("toggle-button-active")
-    }
-
-
 });
