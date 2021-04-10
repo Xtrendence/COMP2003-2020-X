e6a703bd6e8b3d4f7c124abd0bc14af0e352a744
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-04-10 15:38:41 +0100

    dark mode capable

--- web/public/assets/js/create-question-all.js
+++ web/public/assets/js/create-question-all.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     verifySession(localStorage.getItem("sessionToken")).then(result => {
-        
 
+
+    /**
+     * @desc on DOM loaded, it checks to see if localStorage has the key:'theme', and if it does is it's value:'dark'.
+     *      when that is true, it sets the body with an attribute to turn the theme dark.
+     */
+    if(localStorage.getItem('theme') === 'dark') { 
+        document.body.setAttribute('data-theme', 'dark'); 
+    } else { 
+        document.body.removeAttribute('data-theme', 'dark');
+    } 
     }).catch(error => {
         window.location.replace("./login.php");
     });
