dc06722cad914a91b69f2290845ea66822c8cb2e
JoshBent <54994720+JoshBent@users.noreply.github.com>
2021-04-10 16:29:59 +0100

    Dark mode JS added

--- web/public/assets/js/users.js
+++ web/public/assets/js/users.js
@@ -136,4 +136,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     getUsers(from, to);
+
+    /**
+     * @desc on DOM loaded, it checks to see if localStorage has the key:'theme', and if it does is it's value:'dark'.
+     *      when that is true, it sets the body with an attribute to turn the theme dark.
+     */
+    if(localStorage.getItem('theme') === 'dark') {
+        document.body.setAttribute('data-theme', 'dark');
+    } else {
+        document.body.removeAttribute('data-theme', 'dark');
+    }
 });
