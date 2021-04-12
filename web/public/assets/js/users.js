97c2df063696fb0a941a4b70cbdd57cb109d63fa
JoshBent <54994720+JoshBent@users.noreply.github.com>
2021-04-12 15:56:12 +0100

    Update users.js

--- web/public/assets/js/users.js
+++ web/public/assets/js/users.js
@@ -75,6 +75,16 @@ document.addEventListener("DOMContentLoaded", () => {
         getUsers(from, to);
     });
 
+    function deleteCheck(userID){
+        if (confirm("Are you sure you want to delete this user?")){
+            window.location = "./api/users/delete.php?id=" + userID + "";
+        }
+        else{
+            window.alert("User " + userID + " not deleted")
+        }
+    }
+
+
     function createButtons(deleteButton, editButton, profileButton, answerButton, questButton, userID, cellAct, row){
         let editAnchor = document.createElement("a");
         let profileAnchor = document.createElement("a");
@@ -87,7 +97,8 @@ document.addEventListener("DOMContentLoaded", () => {
         deleteButton.innerHTML = '<div class="svg-div"></div><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">\n' +
             '  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>\n' +
             '</svg></div>'
-        editAnchor.href = "/api/users/delete.php" + userID + "";
+        deleteButton.addEventListener("click", deleteCheck)
+        // deleteButton.href = "./api/users/delete.php" + userID + "";
 
         editButton = document.createElement("button");
         editButton.classList.add("action-button");
