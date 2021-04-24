0c2c367515493d313ef720c5d0499b08a0e52829
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-04-24 16:38:49 +0100

    keeps pages scale

--- web/public/create-question.php
+++ web/public/create-question.php
@@ -18,6 +18,7 @@
     <script src="./assets/js/create-question.js"></script>
     <script src="./assets/js/lib/x-notify.js"></script>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create A Question</title>
 </head>
 
