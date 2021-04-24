0f238b7c37426c6513cec4a6fd248a4b7c7625b3
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-04-24 19:53:53 +0100

    the title card has got the first half of its information

--- web/public/assets/js/charts.js
+++ web/public/assets/js/charts.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     //verifySession(localStorage.getItem("sessionToken")).then(result => {
 
+        let url = new URL(window.location.href);
+        let patID = url.searchParams.get("id");
+
+        let titleCard = document.getElementById("user-chart");
+        let startTitle = "Data Analysis - User ";
+        let addID = startTitle.concat(patID);
+        titleCard.innerText = addID;
+
     checkTheme();
     //}).catch(error => {
     //    window.location.replace("./login.php");
