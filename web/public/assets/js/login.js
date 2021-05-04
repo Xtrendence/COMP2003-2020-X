d7893f616b6bcbf1dcd84b0873abee5b61b867f6
Xtrendence <xtrendence@yahoo.com>
2021-05-04 14:16:59 +0100

    Admins can now login.

--- web/public/assets/js/login.js
+++ web/public/assets/js/login.js
@@ -1,131 +1,120 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const Notify = new XNotify("BottomRight");
+	const Notify = new XNotify("BottomRight");
 
-    let apiKey;
-    let apiURL = "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/";
-    let apiAccess;
+	let apiKey = localStorage.getItem("sessionToken");
+	let apiURL = "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/public/api/";
+	let apiAccess;
 
-    let inputAdminUsername = document.getElementById("admin-username");
-    let inputAdminPassword = document.getElementById("admin-password");
+	let inputAdminUsername = document.getElementById("admin-username");
+	let inputAdminPassword = document.getElementById("admin-password");
 
-    let buttonAdminLogin = document.getElementById("admin-login");
+	let buttonAdminLogin = document.getElementById("admin-login");
 
-    buttonAdminLogin.addEventListener("click", async () => {
-        if (!buttonAdminLogin.classList.contains("disabled")) {
-            if (((empty(inputAdminPassword.value)) && (empty(inputAdminUsername.value)))) {
-                let url = new URL(window.location.href);
-                let key = url.searchParams.get("key");
-                if (empty(key)) {
-                    apiKey = empty(inputKey.value) ? window.prompt("Empty API key...") : inputKey.value;
-                } else {
-                    apiKey = key;
-                }
-            }
+	checkSession().then(() => {
+		window.location.replace("./");
+	}).catch(error => {
+		Notify.error({
+			title: "Error",
+			description: error
+		});
+	});
 
-            if (((empty(inputAdminPassword.value)) && (!empty(inputAdminUsername.value))) || ((!empty(inputAdminPassword)) && (empty(inputAdminUsername)))) {
-                Notify.error({
-                    title: "Error",
-                    description: "Admin login details must be entered."
-                });
-            }
+	buttonAdminLogin.addEventListener("click", async () => {
+		if (!empty(inputAdminUsername.value) && !empty(inputAdminPassword.value)) {
+			login(inputAdminUsername.value, inputAdminPassword.value).then(() => {
+				window.location.replace("./");
+			}).catch(error => {
+				Notify.error({
+					title: "Error",
+					description: error
+				});
+			});
+		} else {
+			Notify.error({
+				title: "Error",
+				description: "Please fill out both fields."
+			});
+		}
+	});
 
-            if ((!empty(inputAdminUsername.value)) && (!empty(inputAdminPassword.value))) {
-                await loginAdmin(inputAdminUsername.value, inputAdminPassword.value).catch(e => {
-                    Notify.error({
-                        title: "Error",
-                        description: e
-                    });
-                });
-                attemptStart();
-            }
-        }
-    });
+	function login(username, password) {
+		return new Promise((resolve, reject) => {
+			let xhr = new XMLHttpRequest();
 
-    function loginAdmin(username, password) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
+			xhr.addEventListener("readystatechange", () => {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (validJSON(xhr.responseText)) {
+						let response = JSON.parse(xhr.responseText);
 
-            xhr.addEventListener("readystatechange", () => {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (validJSON(xhr.responseText)) {
-                        let response = JSON.parse(xhr.responseText);
+						if (response.valid) {
+							localStorage.setItem("sessionToken", response.token);
 
-                        if (response.valid) {
-                            apiKey = response.token;
+							Notify.success({
+								title: "Success",
+								description: "You are now logged in as an administrator."
+							});
 
-                            console.log(apiKey);
+							resolve();
+						} else {
+							reject("Login failed.");
+						}
+					} else {
+						reject("Invalid JSON.");
+					}
+				}
+			});
 
-                            Notify.success({
-                                title: "Success",
-                                description: "You are now logged in as an administrator."
-                            });
+			xhr.open("POST", apiURL + "admins/login.php", true);
+			xhr.send(JSON.stringify({ researcher_username: username, researcher_password: password, fcmToken: "-" }));
+		});
+	}
 
-                            log("success", "Logged In As Admin");
+	function validJSON(json) {
+		try {
+			let object = JSON.parse(json);
+			if (object && typeof object === "object") {
+				return true;
+			}
+		}
+		catch (e) { }
+		return false;
+	}
 
-                            resolve();
-                        } else {
-                            reject("Login failed.");
-                        }
-                    } else {
-                        reject("Invalid JSON.");
-                    }
-                }
-            });
+	function checkSession() {
+		return new Promise((resolve, reject) => {
+			if (!empty(apiKey)) {
+				let xhr = new XMLHttpRequest();
 
-            xhr.open("POST", apiURL + "admins/login.php", true);
-            xhr.send(JSON.stringify({ researcher_username: username, researcher_password: password, fcmToken: "-" }));
-        });
-    }
+				xhr.addEventListener("readystatechange", () => {
+					if (xhr.readyState === XMLHttpRequest.DONE) {
+						if (validJSON(xhr.responseText)) {
+							let response = JSON.parse(xhr.responseText);
 
-    function validJSON(json) {
-        try {
-            let object = JSON.parse(json);
-            if (object && typeof object === "object") {
-                return true;
-            }
-        }
-        catch (e) { }
-        return false;
-    }
+							if (response.valid) {
+								localStorage.setItem("sessionToken", response.token);
+								resolve();
+							} else {
+								reject("Login failed.");
+							}
+						} else {
+							reject("Invalid JSON.");
+						}
+					}
+				});
 
-    function attemptStart() {
-        if (empty(apiKey)) {
-            Notify.error({
-                title: "Error",
-                description: "Cannot log in without an API key."
-            });
-        } else {
-            login();
-        }
-    }
+				xhr.open("GET", apiURL + "admins/verify.php?key=" + apiKey, true);
+				xhr.send();
+			}
+		});
+	}
 
-    async function login() {
-        console.log("Logging in...");
-        buttonAdminLogin.classList.add("disabled");
-
-        if (apiKey.includes("admin") || apiKey.includes("researcher")) {
-            log("alert", "Using admin token");
-            apiAccess = "admin";
-        } else {
-            log("alert", "Using development token");
-            apiAccess = "admin";
-        }
-    }
-
-    function log(className, text) {
-        let element = document.createElement("span");
-        element.textContent = text;
-        divOutput.appendChild(element);
-        element.classList.add(className);
-    }
-
-    function empty(value) {
-        if (typeof value === "object" && value !== null && Object.keys(value.length === 0)) {
-            return true;
-        }
-        if (value === null || typeof value === "undefined" || value.toString().trim() === "") {
-            return true;
-        }
-        return false;
-    }
- });
+	function empty(value) {
+		if (typeof value === "object" && value !== null && Object.keys(value.length === 0)) {
+			return true;
+		}
+		if (value === null || typeof value === "undefined" || value.toString().trim() === "") {
+			return true;
+		}
+		return false;
+	}
+});
--- web/public/login.php
+++ web/public/login.php
@@ -1,39 +1,38 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
-
-<head>
-	<link rel="apple-touch-icon" sizes="180x180" href="./assets/img/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon/favicon-16x16.png">
-	<link rel="manifest" href="./assets/img/favicon/site.webmanifest">
-	<link rel="mask-icon" href="./assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-	<link rel="shortcut icon" href="./assets/img/favicon/favicon.ico">
-	<meta name="msapplication-TileColor" content="#2b5797">
-	<meta name="msapplication-config" content="./assets/img/favicon/browserconfig.xml">
-	<meta name="theme-color" content="#a3acd1">
-    <link rel="stylesheet" href="./assets/css/themes.css">
-    <link rel="stylesheet" href="./assets/css/global.css">
-    <link rel="stylesheet" href="./assets/css/login/style.css">
-    <link rel="stylesheet" href="./assets/css/login/resize.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-    <script src="./assets/js/login.js"></script>
-    <title>Login Page</title>
-</head>
-
-<body>
-<div class="wrapper"> <!-- login interface -->
-    <div class="title-card">
-        <span>Admin Login</span>
-    </div>
-    <div class="wide-card">
-        <h1>Username</h1>
-        <input id="admin-username" type="text" placeholder="Admin username...">
-        <h1>Password</h1>
-        <input id="admin-password" type="text" placeholder="Admin password...">
-        <button id="admin-login">Log In</button>
-    </div>
-</div>
-</body>
+	<head>
+		<link rel="apple-touch-icon" sizes="180x180" href="./assets/img/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon/favicon-16x16.png">
+		<link rel="manifest" href="./assets/img/favicon/site.webmanifest">
+		<link rel="mask-icon" href="./assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="shortcut icon" href="./assets/img/favicon/favicon.ico">
+		<meta name="msapplication-TileColor" content="#2b5797">
+		<meta name="msapplication-config" content="./assets/img/favicon/browserconfig.xml">
+		<meta name="theme-color" content="#a3acd1">
+		<link rel="stylesheet" href="./assets/css/themes.css">
+		<link rel="stylesheet" href="./assets/css/global.css">
+		<link rel="stylesheet" href="./assets/css/login/style.css">
+		<link rel="stylesheet" href="./assets/css/login/resize.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="UTF-8">
+		<script src="./assets/js/lib/x-notify.js"></script>
+		<script src="./assets/js/login.js"></script>
+		<title>Login Page</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="title-card">
+				<span>Admin Login</span>
+			</div>
+			<div class="wide-card">
+				<h1>Username</h1>
+				<input id="admin-username" type="text" placeholder="Admin username...">
+				<h1>Password</h1>
+				<input id="admin-password" type="text" placeholder="Admin password...">
+				<button id="admin-login">Log In</button>
+			</div>
+		</div>
+	</body>
 </html>
 
