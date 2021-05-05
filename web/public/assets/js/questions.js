009fabe84c5621d3e2df09ff317f078665094e1a
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-05-05 16:19:47 +0100

    Inserted whole JS code into the VerifySession function.
    This is so that when an admin user is currently logged in with their admin credentials, the session token that contains the details for the logged in user of the web page (whole site) can be recognised. The VerifySession function stems from the "global.js" file. Web page has also been linked to this JS file before any other script within the <head> tag in "questions".php.
    
    Have also made API endpoint link (URI) go from absolute to relative:
    
    xhr.open("GET", "./api/users/read-range.php?from=" + from + "&to=" + to + "&key=" + result.token, true);

--- web/public/assets/js/questions.js
+++ web/public/assets/js/questions.js
@@ -1,88 +1,96 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("123");
-    let wideCard;
-    let cardWrap = document.getElementById("cards");
-    const xhr = new XMLHttpRequest();
-    let layer = document.getElementById("cards");
+    let sessionToken = localStorage.getItem("sessionToken");
 
-    let from = 1;
-    let to = 2;
+    verifySession(sessionToken).then(result => {
+        console.log("123");
+        let wideCard;
+        let cardWrap = document.getElementById("cards");
+        const xhr = new XMLHttpRequest();
+        let layer = document.getElementById("cards");
 
-
-
-    console.log("debug");
-
-    function getQuestions(from, to) {
-        xhr.addEventListener("readystatechange", function () {
-            console.log("hello");
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                let json = xhr.responseText;
-                let users = JSON.parse(json);
-                console.log("hello 2");
-                let keys = Object.keys(users["data"]);
-
-                try {
-                    layer.innerHTML = "";
-                    let question = users["data"];
-                    console.log(keys.length + "look");
-                    for (let i = 0; i < keys.length; i++) {
-
-
-                        /* Variables used for "read" api endpoint field values */
-                        let questionID = question[i]["questionID"];
-                        let q = question[i]["question"];
-                        let questionLim = question[i]["question_charLim"];
-                        let questionType = question[i]["question_type"];
-                        let choices = question[i]["choices"];
-
-                        console.log("debug");
+        let from = 1;
+        let to = 100;
 
 
 
-                        console.log("debug");
+
+
+        function getQuestions(from, to) {
+            xhr.addEventListener("readystatechange", function () {
+                console.log("hello");
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    let json = xhr.responseText;
+                    let users = JSON.parse(json);
+                    console.log("hello 2");
+
+                    if ("data" in users) {
+                        let keys = Object.keys(users["data"]);
+
+                        try {
+                            layer.innerHTML = "";
+                            let question = users["data"];
+                            console.log(keys.length + "look");
+                            for (let i = 0; i < keys.length; i++) {
+
+
+                                /* Variables used for "read" api endpoint field values */
+                                let questionID = question[i]["questionID"];
+                                let q = question[i]["question"];
+                                let questionLim = question[i]["question_charLim"];
+                                let questionType = question[i]["question_type"];
+                                let choices = question[i]["choices"];
 
 
 
-                        /*questionCard(wideCard, questionID, q, questionLim, questionType);
-                        choiceCard(wideCard, questionID, q, questionLim, questionType, choices);*/
+                                /*questionCard(wideCard, questionID, q, questionLim, questionType);
+                                choiceCard(wideCard, questionID, q, questionLim, questionType, choices);*/
 
-                        if (questionType == "choice") {
-
-
-
-                            choiceCard(wideCard, questionID, q, questionLim, questionType, choices);
+                                if (questionType == "choice") {
 
 
-                        } else {
-                            questionCard(wideCard, questionID, q, questionLim, questionType);
+
+                                    choiceCard(wideCard, questionID, q, questionLim, questionType, choices);
+
+
+                                } else {
+                                    questionCard(wideCard, questionID, q, questionLim, questionType);
+                                }
+
+
+
+                            }
+
+                        } catch {
+                            console.log("error");
+
+
                         }
-
-
 
                     }
 
-                } catch {
-                    console.log("error");
+                    else {
+
+                    }
+
+
 
 
                 }
 
-            }
+
+            });
 
 
-        });
+            xhr.open("GET", "./api/questions/read-range.php?from=" + from + "&to=" + to + "&key=" + result.token, true);
+            xhr.send();
 
-        console.log("123");
-        xhr.open("GET", "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/api/questions/read-range.php?from=" + from + "&to=" + to + "&key=8c068d98-874e-46ab-b2a1-5a5eb45a40a6", true);
-        xhr.send();
-
-    }
+        }
 
 
 
 
-    function questionCard(wideCard, questionID, q, questionLim, questionType){
+        function questionCard(wideCard, questionID, q, questionLim, questionType) {
 
             let cardAnchor = document.createElement("a");
 
@@ -90,44 +98,44 @@ document.addEventListener("DOMContentLoaded", () => {
             wideCard.classList.add("question-wide-card");
             wideCard.id = "wideCard";
             wideCard.innerHTML = '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Question ID: <span class="card-text">'+ questionID +'</span> </span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Question: <span class="card-text">'+ q +'</span> </span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Character Limit: <span class="card-text">'+ questionLim +'</span></span>\n' +
-            '        </div>\n' +
-            '\n' +
-            '        <div class="text-margin">\n' +
-            '            <span class="card-heading">Type: <span class="card-text">'+ questionType +'</span> </span>\n' +
-            '        </div>\n' +
-            '\n' +
+                '        <div class="text-margin">\n' +
+                '            <span class="card-heading">Question ID: <span class="card-text">'+ questionID +'</span> </span>\n' +
+                '        </div>\n' +
+                '\n' +
+                '        <div class="text-margin">\n' +
+                '            <span class="card-heading">Question: <span class="card-text">'+ q +'</span> </span>\n' +
+                '        </div>\n' +
+                '\n' +
+                '        <div class="text-margin">\n' +
+                '            <span class="card-heading">Character Limit: <span class="card-text">'+ questionLim +'</span></span>\n' +
+                '        </div>\n' +
+                '\n' +
+                '        <div class="text-margin">\n' +
+                '            <span class="card-heading">Type: <span class="card-text">'+ questionType +'</span> </span>\n' +
+                '        </div>\n' +
+                '\n' +
 
-            '    </div>'
+                '    </div>'
 
 
             cardAnchor.appendChild(wideCard);
             cardWrap.appendChild(cardAnchor);
 
-    }
+        }
 
-    getQuestions(from, to);
+        getQuestions(from, to);
 
-    function choiceCard(wideCard, questionID, q, questionLim, questionType, choices) {
+        function choiceCard(wideCard, questionID, q, questionLim, questionType, choices) {
 
-        try {
-            let cardAnchor = document.createElement("a");
+            try {
+                let cardAnchor = document.createElement("a");
 
-            let printout = Object.keys(choices).length;
+                let printout = Object.keys(choices).length;
 
 
-            wideCard = document.createElement("div");
-            wideCard.classList.add("question-wide-card");
-            wideCard.id = "wideCard";
+                wideCard = document.createElement("div");
+                wideCard.classList.add("question-wide-card");
+                wideCard.id = "wideCard";
 
 
 
@@ -157,43 +165,62 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-        } catch {
-            console.log("error");
+            } catch {
+                console.log("error");
+            }
+
+
         }
 
 
-    }
+
+        document.getElementById("prev-button").addEventListener("click", function () {
+            let xhr = new XMLHttpRequest();
+            layer.innerHTML = "";
+            from = to - 1 ;
+            to = to - 2 ;
+            getQuestions(from, to);
+        });
+
+
+        document.getElementById("next-button").addEventListener("click", function () {
+            let xhr = new XMLHttpRequest();
+            layer.innerHTML = "";
+            from = to + 1;
+            to = to + 2;
+            getQuestions(from, to);
+        });
+
+
+        document.getElementById("ask-button").addEventListener("click", function () {
+
+        });
+
+
+        document.getElementById("ask-button").addEventListener("click", function () {
+
+        });
+
+
+        document.getElementById("ask-button").addEventListener("click", function () {
+
+        });
 
 
 
-    document.getElementById("prev-button").addEventListener("click", function () {
-        let xhr = new XMLHttpRequest();
-        layer.innerHTML = "";
-        from = to - 1 ;
-        to = to - 2 ;
-        getQuestions(from, to);
+        /**
+         * @desc on DOM loaded, it checks to see if localStorage has the key:'theme', and if it does is it's value:'dark'.
+         *      when that is true, it sets the body with an attribute to turn the theme dark.
+         */
+        if(localStorage.getItem('theme') === 'dark') {
+            document.body.setAttribute('data-theme', 'dark');
+        } else {
+            document.body.removeAttribute('data-theme', 'dark');
+        }
+    }).catch(error => {
+        window.location.replace("./login.php");
     });
 
-
-    document.getElementById("next-button").addEventListener("click", function () {
-        let xhr = new XMLHttpRequest();
-        layer.innerHTML = "";
-        from = to + 1;
-        to = to + 2;
-        getQuestions(from, to);
-    });
-
-
-
-    /**
-     * @desc on DOM loaded, it checks to see if localStorage has the key:'theme', and if it does is it's value:'dark'.
-     *      when that is true, it sets the body with an attribute to turn the theme dark.
-     */
-    if(localStorage.getItem('theme') === 'dark') {
-        document.body.setAttribute('data-theme', 'dark');
-    } else {
-        document.body.removeAttribute('data-theme', 'dark');
-    }
 
 
 
