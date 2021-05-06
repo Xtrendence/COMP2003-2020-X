b63a4608a3f0730e1b730e5e397be544c51ef17d
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2021-05-06 17:24:41 +0100

    fixed error with API link

--- app/screens/FallsPage.js
+++ app/screens/FallsPage.js
@@ -62,7 +62,7 @@ export class FallsPage extends Component {
 
 		let patientID = await AsyncStorage.getItem("patientID");
 
-		let endpoint = "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/public/api/diary-entries/create.php" + token;
+		let endpoint = "http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_X/public/api/diary-entries/create.php?key=" + token;
 
 		let body = { patientID:patientID, entry:this.state.diary};
 
@@ -73,7 +73,7 @@ export class FallsPage extends Component {
 			},
 			body: JSON.stringify(body)
 		})
-		.then(() => {
+		.then(() => { 
 			this.setState({diary:null});
 			showMessage({
 				message: "Diary Saved",
