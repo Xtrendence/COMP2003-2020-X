a35bde385e8a3c305ede594fc5705c7fb69a5108
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2021-05-10 21:24:55 +0100

    edit questions tab now functional
    needs some aesthetic fixing

--- web/public/assets/js/answers-search.js
+++ web/public/assets/js/answers-search.js
@@ -346,74 +346,34 @@ document.addEventListener("DOMContentLoaded", () => {
             }
 
             function allQuestionInfo () {
-                let allQuestions = [];
                 let xhr = new XMLHttpRequest();
                 xhr.addEventListener("readystatechange", function() {
                     if (xhr.readyState === XMLHttpRequest.DONE) {
                         try {
                             let json = xhr.responseText;
-                            let ans = JSON.parse(json);
-                            let keys = Object.keys(ans["data"]);
-
-                            let quest = ans["data"];
-
-                            for (let i = 0; i < keys.length; i++) {
-                                let questionId = quest[keys[i]]["questionID"];
-                                allQuestions.push(questionId);
-                            }
-                            console.log(allQuestions);
-                        } catch(error) {
-                            console.trace(error)
-                        }
-                    }
-                });
-                xhr.open("GET", "./api/questions/read-all.php?key=" + sessionToken + "", true);
-                xhr.send();
-                findQuestionInfo(allQuestions)
-            }
-
-            function findQuestionInfo (allQuestions) {
-                let xhr = new XMLHttpRequest();
-                xhr.addEventListener("readystatechange", function() {
-                    if (xhr.readyState === XMLHttpRequest.DONE) {
-                        try {
-                            let json = xhr.responseText;
-                            let ans = JSON.parse(json);
-                            let keys = Object.keys(ans["data"]);
-
-                            recentNoAns.innerHTML = "";
-                            contentNoAns.innerHTML = "";
-                            let answeredQuestions = [];
-                            let quest = ans["data"];
-
-                            for (let i = 0; i < keys.length; i++) {
-                                let questionId = quest[keys[i]]["questionID"];
-                                let answer = quest[keys[i]]["answer"];
-
-                                if (answer != "") {
-                                    answeredQuestions.push(questionId);
-                                }
-                            }
-                            console.log(answeredQuestions);
-
-                            for (let i = 0; i < answeredQuestions.length; i++) {
-                                for (let j = 0; j < allQuestions.length; j++) {
-                                    if (answeredQuestions[i] == allQuestions[j]) {
-                                        allQuestions.splice(j);
+                            let allQuestions = JSON.parse(json).data;
+                            let editable = {};
+                            let keys = Object.keys(allQuestions);
+                            keys.map(key => {
+                                if (allQuestions[key].answer === null || allQuestions[key].answer === "") {
+                                    editable[allQuestions[key].question] = allQuestions[key];
+                                } else {
+                                    if (allQuestions[key].question in editable) {
+                                        delete editable[allQuestions[key].question];
                                     }
                                 }
-                            }
-                            console.log(allQuestions);
-                            for (let i = 0; i < allQuestions.length; i++) {
-                                let questionId = quest[keys[i]]["questionID"];
-                                let question = quest[keys[i]]["question"];
-                                let questionType = quest[keys[i]]["question_type"];
-                                let choices = quest[keys[i]]["choices"];
+                            });
+
+                            let keysOfEditable = Object.keys(editable);
+                            keysOfEditable.map(key => {
+                                let questionId = editable[key].questionID
+                                let question = editable[key].question;
+                                let questionType = editable[key].question_type;
+                                let choices = editable[key].choices;
 
                                 let cardDiv = document.createElement("div");
                                 cardDiv.classList.add("wide-card");
-                                cardDiv.id = i;
-
+    
                                 let titleSpan = document.createElement("span");
                                 titleSpan.classList.add("title-span")
                                 let span = document.createElement("span");
@@ -422,31 +382,31 @@ document.addEventListener("DOMContentLoaded", () => {
                                 if (questionType == "choice") {
                                     let choice = [];
                                     let choiceKeys = Object.keys(choices);
-
+    
                                     for (let j = 0; j < choiceKeys.length; j++){
                                         choice.push(choices[j+1]);
                                     }
                                     let choiceStr = choice.join(",");
-                                    span.innerHTML = ("<br />Question Type: " + questionType + "<br />Choices: " + choiceStr + "<br />Answer: " + answer);
+                                    span.innerHTML = ("<br />Question Type: " + questionType + "<br />Choices: " + choiceStr);
                                 } else {
-                                    let charLim = quest[i]["question_charLim"];
-                                    span.innerHTML = ("<br />Question Type: " + questionType + "<br />Character Limit: " + charLim + "<br />Answer: " + answer);
+                                    let charLim = editable[key].question_charLim;
+                                    span.innerHTML = ("<br />Question Type: " + questionType + "<br />Character Limit: " + charLim);
                                 }
-
+    
                                 let spanContainer = document.createElement("div");
                                 spanContainer.classList.add("text-wrapper");
-
+    
                                 let titleContainer = document.createElement("div");
                                 titleContainer.classList.add("question-wrapper");
-
+    
                                 spanContainer.appendChild(span)
                                 titleContainer.appendChild(titleSpan);
                                 cardDiv.appendChild(titleContainer);
                                 cardDiv.appendChild(spanContainer);
-
+    
                                 let butContainer = document.createElement("div");
                                 butContainer.classList.add("button-wrapper");
-
+    
                                 let delAnchor = document.createElement("a");
                                 let delBut = document.createElement("button");
                                 delBut.classList.add("action-button");
@@ -454,25 +414,25 @@ document.addEventListener("DOMContentLoaded", () => {
                                 delBut.addEventListener('click', function() {
                                     deleteQuestion(questionId);
                                 });
-
+    
                                 delAnchor.appendChild(delBut);
                                 butContainer.appendChild(delAnchor);
-
+    
                                 let editAnchor = document.createElement("a");
                                 let editBut = document.createElement("button");
                                 editBut.classList.add("action-button");
                                 editBut.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"/></svg>';
-
+    
                                 editAnchor.appendChild(editBut);
                                 editAnchor.href = "./edit-question.php?id=" + questionId + "";
                                 butContainer.appendChild(editAnchor);
-
+    
                                 cardDiv.appendChild(butContainer);
                                 contentNoAns.appendChild(cardDiv);
-
+    
                                 let recentQuestion = contentNoAns.lastChild;
                                 recentNoAns.appendChild(recentQuestion);
-                            }
+                            });
                         } catch(error) {
                             console.trace(error)
                         }
