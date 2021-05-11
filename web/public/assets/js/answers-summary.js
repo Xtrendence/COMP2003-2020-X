f648ef8b629b2c6935fefef8f583cf3f73ec1afc
Xtrendence <xtrendence@yahoo.com>
2021-05-11 16:51:23 +0100

    More "Answers Summary" page CSS changes.

--- web/public/assets/js/answers-summary.js
+++ web/public/assets/js/answers-summary.js
@@ -335,7 +335,6 @@ document.addEventListener("DOMContentLoaded", () => {
 							searchWrapper.appendChild(input);
 
 							let searchButton = document.createElement("button");
-							searchButton.classList.add("search-input");
 							searchButton.classList.add("search-button");
 							searchButton.id = "search-button";
 							searchButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>';
@@ -453,16 +452,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 						keys = Object.keys(editable);
 						keys.map(key => {
-							let questionId = editable[key].questionID
+							let questionId = editable[key].questionID;
 							let question = editable[key].question;
 							let questionType = editable[key].question_type;
 							let choices = editable[key].choices;
 
 							let card = document.createElement("div");
 							card.classList.add("new-wide-card");
+							card.classList.add("editable-card");
 
 							let title = document.createElement("span");
-							title.classList.add("title-span")
+							title.classList.add("title-span");
 							let span = document.createElement("span");
 							title.innerHTML = (question);
 							
